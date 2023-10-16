7cf59f73b740fd541c300a6dea070778a5dde8b6
Johna210 <solomonjohna21@gmal.com>
2023-10-16 12:27:23 +0300

    lists

--- lists.py
+++ lists.py
@@ -1,27 +1,22 @@
-from random import randint
-
 if __name__ == '__main__':
     N = int(input())
-
     lst = []
-
     while N > 0:
-        operation = randint(0,7)
-        if operation == 0:
-            lst.append(randint(1,10))
-        elif operation == 1:
+        command = input().split()
+        if command[0] == "insert":
+            lst.insert(int(command[1]),int(command[2]))
+        elif command[0] == "print":
             print(lst)
-        elif operation == 2:
-            lst.pop()
-        elif operation == 3:
-            lst.insert(randint(1,10,1),randint(0,len(lst) - 1,1))
-        elif operation == 4:
-            lst.reverse()
-        elif operation == 5:
+        elif command[0] == "remove":
+            lst.remove(int(command[1]))
+        elif command[0] == "append":
+            lst.append(int(command[1]))
+        elif command[0] == "sort":
             lst.sort()
-        elif operation == 6:
-            if len(lst) > 2:
-                lst.remove(lst[randint(0,len(lst) - 1),randint(0,len(lst) - 1)])
-            else:
-                continue
-        N -= 1
+        elif command[0] == "pop":
+            lst.pop()
+        else:
+            lst.reverse()
+        
+        N-=1
+        
