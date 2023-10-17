3349f3143750d52a142d6c0e8283c192ac1fab63
Johna210 <solomonjohna21@gmal.com>
2023-10-17 13:38:20 +0300

    addSpaces

--- addSpaces.py
+++ addSpaces.py
@@ -1,22 +1,11 @@
-def addSpaces(s, spaces):
-    edited = ""
-    start = 0
-    for i in spaces:
-        edited += s[start:i] + " "
-        start = i
-    edited += s[start:]
-    return edited
+class Solution:
+    def addSpaces(self, s: str, spaces: List[int]) -> str:
+        edited = ""
+        start = 0
 
-
-s = "LeetcodeHelpsMeLearn"
-spaces = [8,13,15]
-
-s2 = "icodeinpython" 
-spaces2 = [1,5,7,9]
-
-s3 = "spacing"
-spaces3 = [0,1,2,3,4,5,6]
-
-print(addSpaces(s,spaces))
-print(addSpaces(s2,spaces2))
-print(addSpaces(s3,spaces3))
+        for i in spaces:
+            edited += s[start:i] + " "
+            start = i
+        edited += s[start:]
+        
+        return edited
