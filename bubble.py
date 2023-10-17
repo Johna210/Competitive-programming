122d4d62472952893659368b1e9b9412d2d31eab
Johna210 <solomonjohna21@gmal.com>
2023-10-17 21:42:56 +0300

    Bubble

--- bubble.py
+++ bubble.py
@@ -13,25 +13,20 @@ import sys
 #
 
 def countSwaps(a):
+    # Write your code here
     count = 0
-    while True: 
-        swapcounter = 0
-        for i in range(len(a)-1):
-            # If the previous number in the list is greater than the next swap them
-            if a[i] > a[i+1]:
-                tmp = a[i]
-                a[i] = a[i+1]
-                a[i+1] = tmp
-
-                swapcounter = 1
-                count += 1
-        # If there is no swap then it means its already sorted     
-        if swapcounter == 0:
-            break
-
+    for i in range(len(a)-1):
+        for j in range(len(a)-i-1):
+            if a[j] > a[j+1]:
+                a[j+1] , a[j] = a[j], a[j+1]
+                count+=1
+    
     print(f"Array is sorted in {count} swaps.")
     print(f"First Element: {a[0]}")
     print(f"Last Element: {a[-1]}")
+
+        
+
 if __name__ == '__main__':
     n = int(input().strip())
 
