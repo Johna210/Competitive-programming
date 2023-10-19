5ebdf32d799c9322e28dac4af9be2a946e0a24a6
Johna210 <solomonjohna21@gmal.com>
2023-10-19 11:47:26 +0300

    corrected insertion_sort1

--- insertion.py
+++ insertion.py
@@ -15,19 +15,33 @@ import sys
 #
 
 def insertionSort1(n, arr):
-    # Write your code here
     value = arr[-1]
-    arr[-1] = arr[-2]
-    for i in range(len(arr)-1,0,-1):
-        if arr[i] > n:
-            arr[i] = arr[i-1]
-        else:
+    for i in range(n-1,-1,-1):
+        if value > arr[i-1]:
             arr[i] = value
+            print(*arr)
+            break
+        else:
+            if i == 0:
+                arr[i] = value
+            else:
+                arr[i] = arr[i-1]
+
         print(*arr)
 
-if __name__ == '__main__':
-    n = int(input().strip())
+# if __name__ == '__main__':
+#     n = int(input().strip())
 
-    arr = list(map(int, input().rstrip().split()))
+#     arr = list(map(int, input().rstrip().split()))
 
-    insertionSort1(n, arr)
+#     insertionSort1(n, arr)
+
+
+num = 10
+arr = [2, 3, 4, 5, 6, 7, 8, 9, 10, 1]
+
+num2 = 5
+arr2 = [2,4,6,8,3]
+
+print(insertionSort1(num2,arr2))
+print(insertionSort1(num,arr))
