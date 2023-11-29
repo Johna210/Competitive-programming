3bf9cafff69f493951ff89c3905630ae113ccc5c
Johna210 <105077710+Johna210@users.noreply.github.com>
2023-11-29 19:16:07 +0300

    Added solution for Palindrome Number

--- week1/palindrome-number.py
+++ week1/palindrome-number.py
@@ -1,13 +1,11 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        x=str(x)
-        left = 0
-        right = len(x) - 1
-        while left < right:
-            if x[left] != x[right]:
-                return False
-            left += 1
-            right -= 1
+        num = x
+        reverse = 0
 
-        return True
-        
+        while x >= 1:
+            reverse *= 10
+            reverse += (x % 10)
+            x = x // 10
+
+        return num == reverse
