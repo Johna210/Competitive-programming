de6d6b977dd472b5180c20312ffa23f70a2873cf
Johna210 <105077710+Johna210@users.noreply.github.com>
2023-12-05 19:01:05 +0300

    Added solution for Multiply Strings

---
+++ week1/multiply-strings.py
@@ -0,0 +1,16 @@
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        number1 = 0
+        number2 = 0
+
+        result = ""
+
+        for i in num1:
+            number1 = number1 * 10 + (ord(i) - 48)
+        
+        for i in num2:
+            number2 = number2 * 10 + (ord(i) - 48)
+
+        result += str(number1*number2)
+
+        return result
