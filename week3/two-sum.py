d6e24b4255a408014722d0bbd653e78e3f8bcfaf
Johna210 <105077710+Johna210@users.noreply.github.com>
2023-12-12 22:27:44 +0300

    Added solution for Two Sum

---
+++ week3/two-sum.py
@@ -0,0 +1,12 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}
+
+        for i in range(len(nums)):
+            opposite = target - nums[i]
+            if opposite in seen:
+                return sorted([i,seen[opposite]])
+            
+            seen[nums[i]] = i
+
+        return []
