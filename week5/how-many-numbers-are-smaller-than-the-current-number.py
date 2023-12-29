f3e6f7a24821ea014146ea09666c78581d5fe329
Johna210 <105077710+Johna210@users.noreply.github.com>
2023-12-29 17:23:02 +0300

    Added solution for How Many Numbers Are Smaller Than the Current Number

---
+++ week5/how-many-numbers-are-smaller-than-the-current-number.py
@@ -0,0 +1,8 @@
+class Solution:
+    def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+        indices = {}
+        for idx, num in enumerate(sorted(nums)):
+            indices.setdefault(num, idx)
+        return [indices[num] for num in nums]
+
+        
