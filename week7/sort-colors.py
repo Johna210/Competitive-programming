6e6723f610570efab56265527e4e10143623b0b0
Johna210 <105077710+Johna210@users.noreply.github.com>
2024-01-09 17:51:49 +0300

    Added solution for Sort Colors

---
+++ week7/sort-colors.py
@@ -0,0 +1,21 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        counts = [0] * 3
+        for i in nums:
+            counts[i] += 1
+
+        counter = 0
+        j = 0
+        while counter < len(counts):
+            if counts[counter] == 0:
+                counter += 1
+            else:
+                nums[j] = counter
+                counts[counter] -= 1
+                j += 1
+
+
+        
