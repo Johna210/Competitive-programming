df36fa132facc459ce6fc929fd509e175fba897e
Yohannes Solomon <105077710+Johna210@users.noreply.github.com>
2024-02-03 20:55:25 +0300

    Add solution for Minimum Recolors to Get K Consecutive Black Blocks

---
+++ week8/leetcode/minimum-recolors-to-get-k-consecutive-black-blocks.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minimumRecolors(self, blocks: str, k: int) -> int:
+
+        counts = []
+        left = 0
+        right = k
+
+        while right <= len(blocks):
+            counts.append(blocks[left:right].count("W"))
+            left += 1
+            right += 1
+
+        return min(counts)
+
+
+        
