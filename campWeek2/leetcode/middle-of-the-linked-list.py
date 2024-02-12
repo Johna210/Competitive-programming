188383b871a3c5bb3c2faffe76733db44ab126b3
Yohannes Solomon <105077710+Johna210@users.noreply.github.com>
2024-02-12 14:57:28 +0300

    Add solution for Middle of the Linked List

---
+++ campWeek2/leetcode/middle-of-the-linked-list.py
@@ -0,0 +1,14 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        first_pointer = second_pointer = head
+        
+        while second_pointer and second_pointer.next:
+            second_pointer = second_pointer.next.next
+            first_pointer = first_pointer.next
+
+        return first_pointer
